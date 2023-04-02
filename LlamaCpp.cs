58d7cc35a93684a04d0349e09663dd36241ab45c
DAN™ <dranger003@gmail.com>
2023-04-02 09:02:22 -0400

    Clean-ups.

---
+++ LlamaCpp.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace LlamaCppDotNet
+{
+    public class LlamaCpp
+    {
+        private delegate string NextInputDelegate();
+        private delegate void NextOutputDelegate(string token);
+
+        [DllImport("main.dll")]
+        private static extern void set_callbacks(NextInputDelegate ni, NextOutputDelegate no);
+
+        [DllImport("main.dll")]
+        private static extern int main(int argc, string[] argv);
+
+        private string[] _args;
+
+        public LlamaCpp(string[] args)
+        {
+            _args = args;
+        }
+
+        public void Run()
+        {
+            var argc = 1 + _args.Length;
+            var argv = new[] { Path.GetFileName(Assembly.GetExecutingAssembly().Location) }.Concat(_args).ToArray();
+
+            set_callbacks(
+                () => Console.ReadLine() ?? String.Empty,
+                Console.Write
+            );
+
+            _ = main(argc, argv);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,49 +1,36 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-internal class Program
+namespace LlamaCppDotNet
 {
-    delegate string next_input_t();
-    delegate void next_output_t(string token);
-
-    [DllImport("main.dll")]
-    static extern void set_callbacks(next_input_t ni, next_output_t no);
-
-    [DllImport("main.dll")]
-    static extern int main(int argc, string[] argv);
-
-    private static void Main(string[] args)
+    internal class Program
     {
-        //args = new[]
-        //{
-        //    "--repeat_penalty", "1.17647",
-        //    "--ctx_size", "2048",
-        //    "--threads", "22",
-        //    "--temp", "0.7",
-        //    "--top_k", "40",
-        //    "--top_p", "0.5",
-        //    "--repeat_last_n", "256",
-        //    "--batch_size", "1024",
-        //    "--n_predict", "2048",
-        //    "--interactive",
-        //    "--reverse-prompt", "User:",
-        //    "--prompt", "Text transcript of a never ending dialog, where User interacts with an AI assistant named ChatLLaMa. ChatLLaMa is helpful, kind, honest, friendly, good at writing and never fails to answer User's requests immediately and with details and precision. There are no annotations like (30 seconds passed...) or (to himself), just what User and ChatLLaMa say aloud to each other. The dialog lasts for years, the entirety of it is shared below. It's 10000 pages long. The transcript only includes text, it does not include markup like HTML and Markdown.",
-        //    "--model", @"C:\LLM_MODELS\LLaMA\7B\ggml-model-f16-q4_0.bin"
-        //};
+        private static void Main(string[] args)
+        {
+            if (args.Any(x => x == "--dev"))
+            {
+                args = new[]
+                {
+                    "--repeat_penalty", "1.17647",
+                    "--ctx_size", "2048",
+                    "--threads", "22",
+                    "--temp", "0.7",
+                    "--top_k", "40",
+                    "--top_p", "0.5",
+                    "--repeat_last_n", "256",
+                    "--batch_size", "1024",
+                    "--n_predict", "2048",
+                    "--interactive",
+                    "--reverse-prompt", "User:",
+                    "--prompt", "Text transcript of a never ending dialog, where User interacts with an AI assistant named ChatLLaMa. ChatLLaMa is helpful, kind, honest, friendly, good at writing and never fails to answer User's requests immediately and with details and precision. There are no annotations like (30 seconds passed...) or (to himself), just what User and ChatLLaMa say aloud to each other. The dialog lasts for years, the entirety of it is shared below. It's 10000 pages long. The transcript only includes text, it does not include markup like HTML and Markdown.",
+                    "--model", @"C:\LLM_MODELS\LLaMA\7B\ggml-model-f16-q4_0.bin"
+                };
+            }
 
-        var argc = 1 + args.Length;
-        var argv = new[] { Path.GetFileName(Assembly.GetExecutingAssembly().Location) }.Concat(args).ToArray();
+            var model = new LlamaCpp(args);
+            model.Run();
 
-        set_callbacks(
-            () => Console.ReadLine() ?? String.Empty,
-            Console.Write
-        );
-
-        _ = main(argc, argv);
-
-        //var builder = WebApplication.CreateBuilder(args);
-        //var app = builder.Build();
-        //app.MapGet("/", () => "Welcome to LLaMA!");
-        //app.Run();
+            //var builder = WebApplication.CreateBuilder(args);
+            //var app = builder.Build();
+            //app.MapGet("/", () => "Welcome to LLaMA!");
+            //app.Run();
+        }
     }
 }
