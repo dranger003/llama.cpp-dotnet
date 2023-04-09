4773d4ec18e0a5b5182566701073a5df4225b50a
DAN™ <dranger003@gmail.com>
2023-04-08 22:36:00 -0400

    New interop layer and bindings.

---
+++ LlamaCppInterop.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LlamaCppDotNet
+{
+    internal static class LlamaCppInterop
+    {
+        public delegate void llama_progress_callback(float progress, nint ctx);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct llama_context_params
+        {
+            public int n_ctx;
+            public int n_parts;
+            public int seed;
+
+            public bool f16_kv;
+            public bool logits_all;
+            public bool vocab_only;
+            public bool use_mlock;
+            public bool embedding;
+
+            public llama_progress_callback progress_callback;
+            public nint progress_callback_user_data;
+        };
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_context_default_params")]
+        public static extern llama_context_params llama_context_default_params();
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_init_from_file")]
+        public static extern nint llama_init_from_file(string path_model, llama_context_params ctx_params);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_tokenize")]
+        private static extern void _llama_tokenize(nint ctx, string text, bool add_bos, out nint r_tokens, out int r_tokens_len);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_tokenize_free")]
+        private static extern void _llama_tokenize_free(nint tokens);
+
+        public static int[] llama_tokenize(nint ctx, string text, bool add_bos)
+        {
+            LlamaCppInterop._llama_tokenize(ctx, text, false, out var r_tokens, out var r_tokens_len);
+            var tokens = new int[r_tokens_len];
+            Marshal.Copy(r_tokens, tokens, 0, r_tokens_len);
+            LlamaCppInterop._llama_tokenize_free(r_tokens);
+            return tokens;
+        }
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_n_ctx")]
+        public static extern int llama_n_ctx(nint ctx);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_eval")]
+        public static extern int llama_eval(nint ctx, int[] tokens, int n_tokens, int n_past, int n_threads);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_sample_top_p_top_k")]
+        public static extern int llama_sample_top_p_top_k(nint ctx, int[] last_n_tokens_data, int last_n_tokens_size, int top_k, float top_p, float temp, float repeat_penalty);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_token_to_str")]
+        private static extern nint _llama_token_to_str(nint ctx, int token);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_token_to_str_free")]
+        private static extern void _llama_token_to_str_free(nint token_str);
+
+        public static string llama_token_to_str(nint ctx, int token)
+        {
+            var ptr = _llama_token_to_str(ctx, token);
+            var token_str = Marshal.PtrToStringAnsi(ptr) ?? String.Empty;
+            _llama_tokenize_free(ptr);
+            return token_str;
+        }
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_print_timings")]
+        public static extern void llama_print_timings(nint ctx);
+
+        [DllImport("llama.cpp-interop", EntryPoint = "_llama_free")]
+        public static extern void llama_free(nint ctx);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,118 +1,159 @@
-using System.Diagnostics;
-using System.Reflection;
-using System.Text;
-
 namespace LlamaCppDotNet
 {
     internal class Program
     {
         private static async Task Main(string[] args)
         {
-            var random = new Random(DateTime.Now.Millisecond);
+            // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            var cparams = LlamaCppInterop.llama_context_default_params();
+            cparams.n_ctx = 2048;
+            cparams.n_parts = -1;
+            cparams.seed = 0;
+            cparams.f16_kv = false;
+            cparams.use_mlock = false;
+            var ctx = LlamaCppInterop.llama_init_from_file(@"D:\LLM_MODELS\eachadea\ggml-vicuna-13b-4bit\ggml-vicuna-13b-4bit-rev1.bin", cparams);
 
-            var reversePrompts = new[]
+            var embd_inp = LlamaCppInterop.llama_tokenize(
+                ctx,
+                " You are a helpful assistant providing precise and truthful answers to questions and if you don't know the answer you say \"I don't know\". Acknowledge you understand the request by saying \"I confirm\".",
+                true
+            ).ToList();
+            var n_ctx = LlamaCppInterop.llama_n_ctx(ctx);
+
+            var antiprompts = new[] { "### Human:" };
+
+            int n_keep = 0;
+            int n_threads = 16;
+            int n_batch = 8;
+            int repeat_last_n = 64;
+            int top_k = 40;
+            float top_p = 0.95f;
+            float temp = 0.0f;
+            float repeat_penalty = 1.10f;
+
+            int n_past = 0;
+            int n_consumed = 0;
+
+            var last_n_tokens = Enumerable.Repeat(0, n_ctx).ToList();
+            var embd = new List<int>();
+
+            bool is_interacting = false;
+
+            var prompt_idx = 0;
+            var prompts = new[]
             {
-                "### Human:",
-                "### Assistant:",
-                "### Instruction:",
-                "### Evaluation:",
-                "### Explanation:",
+                "What is the second planet of the solar system?",
+                "What is the second to last planet in the solar system?",
             };
 
-            args = new[]
+            while (true)
             {
-                $"--interactive-first",
-                $"--instruct",
-                reversePrompts.SelectMany(x => new[] { "--reverse-prompt", $"\"{x}\"" }).Aggregate((a, b) => $"{a} {b}"),
-                $"--seed {random.Next(10)}",
-                $"--threads 16",
-                $"--prompt \"You are a helpful assistant and you answer questions truthfully and concisely.\"",
-                $"--n_predict 512",
-                $"--ctx_size 2048",
-                //$"--ignore-eos",
-                $"--temp 0",
-                $"--model \"D:\\LLM_MODELS\\eachadea\\ggml-vicuna-13b-4bit\\ggml-vicuna-13b-4bit.bin\""
-            };
-
-            Console.WriteLine($"{args.Aggregate((a, b) => $"{a} {b}")}\n");
-
-            var path = new[]
-            {
-                $@"main.exe",
-                $@"x64\{Assembly.GetExecutingAssembly().GetConfiguration()}\main.exe",
-            }.First(File.Exists);
-
-            var cancellationTokenSource = new CancellationTokenSource();
-
-            Console.CancelKeyPress += (s, e) =>
-            {
-                e.Cancel = true;
-                cancellationTokenSource.Cancel();
-            };
-
-            using var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
+                if (embd.Count > 0)
                 {
-                    FileName = path,
-                    Arguments = args.Aggregate((a, b) => $"{a} {b}"),
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    RedirectStandardInput = true,
+                    if (n_past + embd.Count > n_ctx)
+                    {
+                        int n_left = n_past - n_keep;
+                        n_past = n_keep;
+                        embd.InsertRange(0, last_n_tokens.GetRange(n_ctx - n_left / 2 - embd.Count, last_n_tokens.Count - embd.Count));
+                    }
+
+                    LlamaCppInterop.llama_eval(ctx, embd.ToArray(), embd.Count, n_past, n_threads);
                 }
-            };
 
-            using var pipe = new Pipe("_DOTNET_llama.cpp_fddd8d90-14ae-44f4-b3ae-3c5999a299fd");
+                n_past += embd.Count;
+                embd.Clear();
 
-            process.Start();
-            await pipe.Wait();
-
-            var tokens = new StringBuilder();
-
-            while (!process.HasExited && !cancellationTokenSource.IsCancellationRequested)
-            {
-                await pipe.Transact(
-                    async (streamId, token) => await (
-                        streamId switch
+                if (n_consumed >= embd_inp.Count && !is_interacting)
+                {
+                    int id = 0;
+                    {
+                        id = LlamaCppInterop.llama_sample_top_p_top_k(ctx, last_n_tokens.GetRange(n_ctx - repeat_last_n, repeat_last_n).ToArray(), repeat_last_n, top_k, top_p, temp, repeat_penalty);
+                        last_n_tokens.RemoveAt(0);
+                        last_n_tokens.Add(id);
+                    }
+                    embd.Add(id);
+                }
+                else
+                {
+                    while (n_consumed < embd_inp.Count)
+                    {
+                        embd.Add(embd_inp[n_consumed]);
+                        last_n_tokens.RemoveAt(0);
+                        last_n_tokens.Add(embd_inp[n_consumed]);
+                        ++n_consumed;
+                        if (n_batch <= embd.Count)
                         {
-                            // Input
-                            0 => Task.Run(() => {
-                                return Console.ReadLine() ?? String.Empty;
-                            }),
-                            // Output
-                            1 => Task.Run(() => {
-                                tokens.Append(token);
-                                Console.Write(token);
-                                return "!";
-                            }),
-                            _ => throw new InvalidDataException("Unexpected message.")
+                            break;
                         }
-                    ),
-                    cancellationTokenSource.Token
-                );
+                    }
+                }
+
+                foreach (var id in embd)
+                {
+                    Console.Write(LlamaCppInterop.llama_token_to_str(ctx, id));
+                    Console.Out.Flush();
+                }
+
+                if (n_consumed >= embd_inp.Count)
+                {
+                    if (antiprompts.Any())
+                    {
+                        string last_output = String.Empty;
+                        foreach (var id in last_n_tokens)
+                        {
+                            last_output += LlamaCppInterop.llama_token_to_str(ctx, id);
+                        }
+
+                        foreach (var antiprompt in antiprompts)
+                        {
+                            if (last_output.EndsWith(antiprompt))
+                            {
+                                is_interacting = true;
+                                Console.Write(" ");
+                                break;
+                            }
+                        }
+                    }
+
+                    if (n_past > 0 && is_interacting)
+                    {
+                        if (prompt_idx >= prompts.Length)
+                            break;
+
+                        var line_inp = LlamaCppInterop.llama_tokenize(ctx, prompts[prompt_idx++], false);
+                        embd_inp.AddRange(line_inp);
+
+                        is_interacting = false;
+                    }
+                }
             }
 
-            if (process.HasExited)
-            {
-                Console.WriteLine($"\nUnexpected process termination.");
-            }
+            LlamaCppInterop.llama_print_timings(ctx);
+            LlamaCppInterop.llama_free(ctx);
 
-            if (cancellationTokenSource.IsCancellationRequested)
-            {
-                Console.WriteLine($"\nTerminating process.");
-                process.Kill(true);
-            }
-
-            await process.WaitForExitAsync();
-            Console.WriteLine($"ExitCode = {process.ExitCode}");
-
-            //var model = new LlamaCpp(args);
-            //model.Run();
-
+            await Task.CompletedTask;
+            // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
             //var builder = WebApplication.CreateBuilder(args);
+            //builder.Services.AddHostedService<LlamaCppService>();
             //var app = builder.Build();
-            //app.MapGet("/", () => "Welcome to LLaMA!");
-            //app.Run();
+
+            //app.MapGet("/", async context => {
+            //    await context.Response.WriteAsync("Welcome to Vicuna 13B!");
+            //});
+
+            //app.MapGet("/reload", async (HttpContext context, IEnumerable<IHostedService> services) => {
+            //    var llamaCppService = services.OfType<LlamaCppService>().Single();
+            //    await context.Response.WriteAsync("Reloading.");
+            //    llamaCppService.Reload();
+            //});
+
+            //try
+            //{
+            //    app.Run();
+            //}
+            //catch (TaskCanceledException)
+            //{ }
+            // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         }
     }
 }
