641fe8f496de8cc221138a86efdca5bd2f39fcf8
DAN™ <dranger003@gmail.com>
2023-04-23 12:43:13 -0400

    Add Windows build clean-up script.

---
+++ clean.cmd
@@ -0,0 +1,4 @@
+@echo off
+for /d /r . %%d in (bin) do @if exist "%%d" rmdir /s /q "%%d"
+for /d /r . %%d in (obj) do @if exist "%%d" rmdir /s /q "%%d"
+for /d /r . %%d in (x64) do @if exist "%%d" rmdir /s /q "%%d"
