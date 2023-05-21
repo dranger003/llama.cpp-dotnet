34017c661b5a236ffaf8da35e3e57fa08f8935db
DAN™ <dranger003@gmail.com>
2023-05-21 19:55:43 -0400

    Update clean-up script.

--- clean.cmd
+++ clean.cmd
@@ -1,4 +1,11 @@
 @echo off
+cd llama.cpp
+git clean -fdx
+cd ..\bert.cpp
+git clean -fdx
+cd ggml
+git clean -fdx
+cd ..\..
 for /d /r . %%d in (bin) do @if exist "%%d" rmdir /s /q "%%d"
 for /d /r . %%d in (obj) do @if exist "%%d" rmdir /s /q "%%d"
 for /d /r . %%d in (x64) do @if exist "%%d" rmdir /s /q "%%d"
