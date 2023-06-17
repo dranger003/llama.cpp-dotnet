a8f2138cbadb973f1724389f5044f2cac8c11a51
DAN™ <dranger003@gmail.com>
2023-06-17 12:38:38 -0400

    Updated clean-up script.

--- clean.cmd
+++ clean.cmd
@@ -1,7 +1,5 @@
 @echo off
-cd llama.cpp
-git clean -fdx
-cd ..
+git submodule foreach --recursive git clean -fdx
 for /d /r . %%d in (bin) do @if exist "%%d" rmdir /s /q "%%d"
 for /d /r . %%d in (obj) do @if exist "%%d" rmdir /s /q "%%d"
 for /d /r . %%d in (x64) do @if exist "%%d" rmdir /s /q "%%d"
