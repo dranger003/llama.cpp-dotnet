501de6b87fd9c739bda80ac3d7fd187951c45b06
DAN™ <dranger003@gmail.com>
2023-07-25 14:05:27 -0400

    Clean unused enum.

--- LlamaCppWeb/LlamaCppSessionManager.cs
+++ LlamaCppWeb/LlamaCppSessionManager.cs
@@ -1,10 +1,5 @@
-using System.Text.Json.Serialization;
-
 namespace LlamaCppLib
 {
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public enum LlamaCppSessionAction { Create }
-
     public class LlamaCppSessionManager
     {
         public IDictionary<Guid, LlamaCppSession> Sessions { get; private set; } = new Dictionary<Guid, LlamaCppSession>();
