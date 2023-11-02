04118da4a062efd5397d27716b321c896f572f5c
DAN™ <dranger003@gmail.com>
2023-11-02 11:51:30 -0400

    Add UTF-8 assembler.

--- LlamaCppCli/Program.cs
+++ LlamaCppCli/Program.cs
@@ -82,7 +82,7 @@ namespace LlamaCppCli
                 requests.Add(new Request(llama_n_ctx(ctx), tokens) { Id = id });
 
             var stream = true;
-            var pieces_buf = new List<byte[]>();
+            var utf8Assembler = new Utf8Assembler();
 
             while (true)
             {
@@ -193,15 +193,7 @@ namespace LlamaCppCli
                             request.Tokens[request.PosToken++] = token;
 
                             if (stream)
-                            {
-                                var piece_bytes = llama_token_to_piece(mdl, token).ToArray();
-                                pieces_buf.Add(piece_bytes);
-                                if (pieces_buf.SelectMany(x => x).ToArray().TryGetUtf8String(out var piece_str))
-                                {
-                                    Console.Write(piece_str);
-                                    pieces_buf.Clear();
-                                }
-                            }
+                                Console.Write(utf8Assembler.Consume(llama_token_to_piece(mdl, token)));
 
                             if (request.T1 == default)
                                 request.T1 = DateTime.Now;
@@ -405,5 +397,55 @@ namespace LlamaCppCli
 
         // IEquatable<T>
         public bool Equals(Request? other) => other?.Id == this.Id;
+    }
+
+    internal class Utf8Assembler
+    {
+        private List<byte> _buffer = new();
+
+        public string Consume(Span<byte> bytes)
+        {
+            var result = new StringBuilder();
+
+            _buffer.AddRange(bytes.ToArray());
+            while (_buffer.Count > 0)
+            {
+                var validUtf8Length = _FindValidUtf8SequenceLength(_buffer.ToArray());
+                if (validUtf8Length == 0)
+                    break;
+
+                result.Append(Encoding.UTF8.GetString(_buffer.GetRange(0, validUtf8Length).ToArray()));
+                _buffer.RemoveRange(0, validUtf8Length);
+            }
+
+            return result.ToString();
+        }
+
+        private int _FindValidUtf8SequenceLength(byte[] bytes)
+        {
+            var index = 0;
+            while (index < bytes.Length)
+            {
+                var byteCount = _Utf8ByteCount(bytes[index]);
+                if (index + byteCount > bytes.Length)
+                    break;
+
+                index += byteCount;
+            }
+
+            return index;
+        }
+
+        private int _Utf8ByteCount(byte b)
+        {
+            return b switch
+            {
+                _ when (b & 0x80) == 0x00 => 1,     // 1-byte character
+                _ when (b & 0xE0) == 0xC0 => 2,     // 2-byte character
+                _ when (b & 0xF0) == 0xE0 => 3,     // 3-byte character
+                _ when (b & 0xF8) == 0xF0 => 4,     // 4-byte character
+                _ => 0                              // UTF-8 start byte invalid
+            };
+        }
     }
 }
