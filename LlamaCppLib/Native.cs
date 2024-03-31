8c1a81be55c90e59b54712a3c6034450ea51fcf9
DAN™ <dranger003@gmail.com>
2024-03-31 07:49:17 -0400

    Sync API.

--- LlamaCppLib/Native.cs
+++ LlamaCppLib/Native.cs
@@ -32,13 +32,12 @@ namespace LlamaCppLib
             GGML_TYPE_F16 = 1,
             GGML_TYPE_Q4_0 = 2,
             GGML_TYPE_Q4_1 = 3,
-            //GGML_TYPE_Q4_2 = 4, // removed
-            //GGML_TYPE_Q4_3 = 5, // removed
+            // GGML_TYPE_Q4_2 = 4, // removed
+            // GGML_TYPE_Q4_3 = 5, // removed
             GGML_TYPE_Q5_0 = 6,
             GGML_TYPE_Q5_1 = 7,
             GGML_TYPE_Q8_0 = 8,
             GGML_TYPE_Q8_1 = 9,
-            // k-quantizations
             GGML_TYPE_Q2_K = 10,
             GGML_TYPE_Q3_K = 11,
             GGML_TYPE_Q4_K = 12,
@@ -53,9 +52,12 @@ namespace LlamaCppLib
             GGML_TYPE_IQ3_S = 21,
             GGML_TYPE_IQ2_S = 22,
             GGML_TYPE_IQ4_XS = 23,
-            GGML_TYPE_I8,
-            GGML_TYPE_I16,
-            GGML_TYPE_I32,
+            GGML_TYPE_I8 = 24,
+            GGML_TYPE_I16 = 25,
+            GGML_TYPE_I32 = 26,
+            GGML_TYPE_I64 = 27,
+            GGML_TYPE_F64 = 28,
+            GGML_TYPE_IQ1_M = 29,
             GGML_TYPE_COUNT,
         };
 
